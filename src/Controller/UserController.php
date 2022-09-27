74c1f93e123a6571a036eeded837b71e82b7d684
Sylvie Cattoire <cattoiresylvie.pro@gmail.com>
2022-09-27 18:19:58 +0200

    Login Authenticator ok

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -10,11 +10,12 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class UserController extends AbstractController
 {
     #[Route('/register', name: 'user_register')]
-    public function signIn(Request $request, UserPasswordHasherInterface $hasher, UserRepository $userRepository): Response
+    public function signin(Request $request, UserPasswordHasherInterface $hasher, UserRepository $userRepository): Response
     {
         $user = new User();
         $form = $this->createForm(RegisterType::class, $user);
@@ -36,5 +37,31 @@ class UserController extends AbstractController
         return $this->render('user/register.html.twig', [
             'form' => $form->createView()
         ]);
+    }
+
+
+    #[Route('/login', name: 'user_login')]
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        if ($this->getUser()) {
+            return $this->redirectToRoute('main_index');
+        }
+
+        // Get the login error if there is one :
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // Last username entered by the user :
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('user/login.html.twig', [
+            'lastUsername' => $lastUsername,
+            'error' => $error
+        ]);
+    }
+
+
+    #[Route(path: '/logout', name: 'user_logout')]
+    public function logout(): void
+    {
+        // This method can be blank - it will be intercepted by the logout key on your firewall
     }
 }
