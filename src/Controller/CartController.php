08db1660c7eddfcadab25a9de0bd26a848f95341
guillaume <g.vaudet@hotmail.fr>
2022-10-01 23:16:20 +0200

    cartController done, need to fix redirection when click on add icon and fix remove product in cart need to know if we keep it or remove product by product

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -2,27 +2,76 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionBagInterface;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/panier', name: 'cart_')]
+
 class CartController extends AbstractController
 {
+    // public function __construct(private SessionInterface $session)
+    // {
+    // }
+
     #[Route('', name: 'index')]
-    public function index(): Response
+    public function index(SessionInterface $session, ProductRepository $productRepository): Response
     {
-        return $this->render('cart/index.html.twig', [
-            'controller_name' => 'CartController',
+    $cart = $session->get('cart', []);
+
+    $cartWithDataProduct = [];
+    
+    foreach($cart as $id => $quantity){
+        $cartWithDataProduct[] =[
+            'product' => $productRepository->find($id), 
+            'quantity' => $quantity
+        ];
+        
+    }
+
+    $total = 0; 
+    
+    foreach($cartWithDataProduct as $item){
+        $totalItem = $item['product']->getunitPrice() * $item['quantity'];
+        $total += $totalItem;
+    }
+
+        return $this->render('/cart/index.html.twig', [
+            'items' => $cartWithDataProduct,
+            'total' => $total,
         ]);
     }
 
     #[Route('/add/{id}', name: 'add')]
     public function add($id, SessionInterface $session)
-    {
-        $session->set('panier', 3);
+    {        
+        $cart = $session->get('cart', []); 
+        
+        if(!empty($cart[$id])){
+            $cart[$id]++;
+        }else{
+            $cart[$id] = 1;
+        }
 
-        dd($session);
+        $session->set('cart', $cart);
+
+        return $this->redirectToRoute('cart_index');
+    }
+
+    #[Route('/remove/{id}', name: 'remove')]
+    public function remove($id, SessionInterface $session){
+        $cart = $session->get('cart', []);
+
+        if(!empty($cart[$id])){
+            unset($cart[$id]);
+        }
+
+        $session->set('cart', $cart);
+
+        return $this->redirectToRoute('cart_index');
     }
 }
