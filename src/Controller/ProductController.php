983d0e59da31de22e7c3c24445b1a3f010a3025f
Sylvie Cattoire <cattoiresylvie.pro@gmail.com>
2022-10-05 15:03:29 +0200

    fix images and  add alias in database

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
 use App\Repository\ProductRepository;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -11,19 +14,28 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class ProductController extends AbstractController
 {
     #[Route('', name: 'list')]
-    public function list(ProductRepository $productRepository): Response
+    public function list(ProductRepository $productRepository, Request $request, PaginatorInterface $paginator): Response
     {
         $products = $productRepository->findAll();
+
+        $products = $paginator->paginate(
+        $products, // Requête contenant les données à paginer
+        $request->query->getInt('page', 1), // Numéro de la page en cours > passé dans l'URL (1 si aucune page)
+        10 // Nombre de résultats par page
+    );
         
         return $this->render('product/list.html.twig', [
             'products' => $products
         ]);
     }
 
-    #[Route('/{id}/{label}', name: 'single')]
-    public function single($id, ProductRepository $productRepository): Response
+    #[Route('/{alias}', name: 'single')]
+    public function single(Product $product = null): Response
     {
-        $product = $productRepository->find($id);
+        if ($product === null) {
+            // Modifier par un message d'erreur en cas de produit non trouvé (ou qui n'existe plus)
+            dd('PRODUIT NULL');
+        }
 
         return $this->render('product/single.html.twig', [
             'product' => $product
