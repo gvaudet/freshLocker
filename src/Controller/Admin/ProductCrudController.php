23d351e517845af95f14887b3a3cedee3d743550
guillaume <g.vaudet@hotmail.fr>
2022-10-06 22:10:21 +0200

    wrong branch commit to change in new feat branch

--- src/Controller/Admin/ProductCrudController.php
+++ src/Controller/Admin/ProductCrudController.php
@@ -7,7 +7,10 @@ use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\MoneyField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\CollectionField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\SlugField;
 
 class ProductCrudController extends AbstractCrudController
 {
@@ -29,10 +32,15 @@ class ProductCrudController extends AbstractCrudController
     {
         return [
             TextField:: new('label', 'Nom du produit :'),
+            SlugField::new('alias')->setTargetFieldName('label'),
+            AssociationField::new('producers'),
+            AssociationField::new('category'),
+            AssociationField::new('conditioning'),
             TextareaField:: new('description', 'Description :'),
             TextField::new('photo', 'chemin de l\'image du produit:'),
-            MoneyField::new('unitPrice', 'Prix unitaire')->setCurrency('EUR'),
-            TextField::new('conversionFactor', 'Facteur de conversion'),
+            MoneyField::new('unitPrice', 'Prix unitaire')->setCurrency('EUR'), 
+            // Alias => Slug
+
         ];
     }
 }
